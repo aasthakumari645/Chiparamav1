c096a32dcd284b16ad60033b70efdfb6190aeeb1
aasthakumari645 <aasthabitcse25@gmail.com>
2025-11-05 02:17:30 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,16 +2,12 @@ import React from "react";
 import Hero from "../components/Hero";
 
 import Contact from "../components/Contact";
-import InnovationHub from "../components/InnovationHub";
-import ExpertiseShowcase from "../components/ExpertiseShowcase";
 
-import TestimonialsSection from "../components/TestimonialsSection";
-import LogoBand from "../components/FlowingBand";
-import CountUp from "../components/CountUp";
-import WorkWithUsSection from "../components/WorkWithUsSection";
+
+
 import Services from "../components/Services";
 import Solutions from "../components/OurSolutions";
-import IndustriesWeServe from "../components/IndustriesWeServe";
+
 import BlogSection from "../components/Blogs";
 import CompanyDetailsSection from "../components/CompanyDetails";
 
